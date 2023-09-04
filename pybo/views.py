735941c790ee033f3e2b95cb9ca453f8a78f7f7b
minerva421 <minerva421.ai@gmail.com>
2023-09-04 17:40:41 +0900

    질문 삭제 추가

--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,8 @@ from .forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -58,3 +60,35 @@ def question_create(request):
         form = QuestionForm()
     context = {"form": form}
     return render(request, "pybo/question_form.html", context)
+
+
+@login_required(login_url="common:login")
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, "수정권한이 없습니다")
+        return redirect("pybo:detail", question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        print(f"form : {form}")
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect("pybo:detail", question_id=question_id)
+    else:
+        form = QuestionForm(instance=question)
+        print(f"form : {form}")
+    context = {"form": form}
+    return render(request, "pybo/question_form.html", context)
+
+
+@login_required(login_url="common:login")
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, "삭제권한이 없습니다")
+        return redirect("pybo:detail", question_id=question.id)
+    question.delete()
+    print(request, "삭제하였습니다")
+    return redirect("pybo:index")
