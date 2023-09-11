409a47af8f29ed447697fccd3f8c8adaea2acdc6
minerva421 <minerva421.ai@gmail.com>
2023-09-12 02:10:48 +0900

    제목 검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from ..models import Question
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
+    print(f"request : {request}")
     page = request.GET.get("page", "1")  # 페이지
-    question_list = Question.objects.order_by("-create_date")
+    query = request.GET.get("kw")
+
+    if query:
+        question_list = Question.objects.filter(subject__icontains=query).order_by(
+            "-create_date"
+        )
+    else:
+        question_list = Question.objects.order_by("-create_date")
+
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)  # 몇페이지 보여줄건지
     context = {"question_list": page_obj}
